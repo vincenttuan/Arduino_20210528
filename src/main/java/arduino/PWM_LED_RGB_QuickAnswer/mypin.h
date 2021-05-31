43e8ee62e93f63fa100a49195b52f18b2afbc8fb
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-05-31 16:39:31 +0800

    Delete src/main/java/arduino/PWM_LED_RGB_QuickAnswer directory

--- src/main/java/arduino/PWM_LED_RGB_QuickAnswer/mypin.h
+++
@@ -1,8 +0,0 @@
-#define LED_BLUE_PIN     9    // LED BLUE   燈的接腳( 9 = PIN(PWM))
-#define LED_RED_PIN      10   // LED RED    燈的接腳(10 = PIN(PWM))
-#define LED_GEEEN_PIN    11   // LED GEEEN  燈的接腳(11 = PIN(PWM))
-#define BUTTON_GREEN_PIN 7    // 搶答按鈕的接腳(PIN)
-#define BUTTON_BLUE_PIN  12   // 搶答按鈕的接腳(PIN)
-#define BUZZER_PIN       8    // Buzzer的接腳(PIN)
-#define DELAY            100  // 暫停時間 ms
-#define WIN_COUNT        2    // 贏家次數
