a19fc7f91619b85d443fafae5f5aac973f57cb00
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-05-31 16:39:49 +0800

    Add files via upload

---
+++ src/main/java/arduino/PWM_LED_RGB_QuickAnswer/mypin.h
@@ -0,0 +1,13 @@
+#define LED_BLUE_PIN     9    // LED BLUE   燈的接腳( 9 = PIN(PWM))
+#define LED_RED_PIN      10   // LED RED    燈的接腳(10 = PIN(PWM))
+#define LED_GEEEN_PIN    11   // LED GEEEN  燈的接腳(11 = PIN(PWM))
+#define BUTTON_GREEN_PIN 7    // 搶答按鈕的接腳(PIN)
+#define BUTTON_BLUE_PIN  12   // 搶答按鈕的接腳(PIN)
+#define BUZZER_PIN       8    // Buzzer的接腳(PIN)
+#define LED_GEEEN_1_PIN  2    // GEEEN_1_PIN
+#define LED_GEEEN_2_PIN  3    // GEEEN_2_PIN
+#define LED_BLUE_1_PIN   4    // BLUE_1_PIN
+#define LED_BLUE_2_PIN   5    // BLUE_2_PIN
+
+#define DELAY            100  // 暫停時間 ms
+#define WIN_COUNT        2    // 贏家次數
