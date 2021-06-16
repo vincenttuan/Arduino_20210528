e7870fa839581d68d8a7c599e93f55f480e84e00
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-06-16 10:31:49 +0800

    Add files via upload

---
+++ src/main/java/arduino/RFID_1_ReadID/RFID_1_ReadID.ino
@@ -0,0 +1,33 @@
+#include <SPI.h>
+#include <RFID.h>
+#define RST_PIN 9
+#define SDA_PIN 10
+#define BUZEER_PIN 8
+
+RFID rfid(SDA_PIN, RST_PIN);
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();
+  rfid.init();
+  Serial.println("Ready");
+}
+
+void loop() {
+  if(rfid.isCard() && rfid.readCardSerial()) { // 偵測到卡
+      tone(BUZEER_PIN, 976, 200);
+      Serial.println("Card found");
+      String cardId = ""; // 卡片 id
+      for(int i=0;i<5;i++) {
+        //Serial.print(rfid.serNum[i], HEX);
+        //Serial.print(", ");
+        cardId += String(rfid.serNum[i], HEX); // 組合前5個id內容
+      }
+      Serial.print("cardId: ");
+      Serial.println(cardId);
+      Serial.println();
+      delay(1000);
+  }
+
+  rfid.halt(); // 進入休眠
+}
