4b12eae6f5327222c0756e0d7a9eba8612cc6164
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-06-21 13:58:01 +0800

    Add files via upload

--- src/main/java/arduino/BT_IO/BT_IO.ino
+++ src/main/java/arduino/BT_IO/BT_IO.ino
@@ -7,20 +7,30 @@
   VCC: +5v
 */
 #include <SoftwareSerial.h>
+#define BUTTON_PIN 7
 
 SoftwareSerial BTSerial(5, 6); // (RX, TX)
 
 void setup() {
   Serial.begin(9600);   // 0, 1 接腳
   BTSerial.begin(9600); // 5, 6 接腳 (9600 或 38400)
+  pinMode(BUTTON_PIN, INPUT);
 }
 
 void loop() {
   // 檢查是否有資料透過藍芽傳進來
   if(BTSerial.available()) {
-      String data = BTSerial.readString();
-      Serial.print("BTSerial: ")
-      Serial.println(data)
+      String data = BTSerial.readString(); // 讀取藍芽傳來的資料
+      Serial.print("BTSerial Receive: ");
+      Serial.println(data);
+  }
+  // 透過藍芽將資料送給 App
+  if(digitalRead(BUTTON_PIN) == LOW) {
+    int n = random(0, 100);
+    BTSerial.println(n); // 將資料透過藍芽送出
+    Serial.print("Serial Send: ");
+    Serial.println(n);
+    delay(300);
   }
   delay(200);
 
